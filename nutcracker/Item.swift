2d93f6b4671a2c90448bf3e6c4007580a4610268
Vlad Vladov <divaltor@pm.me>
2026-02-15 17:04:11 +0200

    Initial Commit

---
+++ nutcracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  nutcracker
+//
+//  Created by Vlad on 15.02.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ nutcrackerTests/nutcrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  nutcrackerTests.swift
+//  nutcrackerTests
+//
+//  Created by Vlad on 15.02.26.
+//
+
+import Testing
+@testable import nutcracker
+
+struct nutcrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
